357b0f22c6d2b83bfa7be25a0b503ea26401a155
Yana Evstegneeva <yanka.evste@gmail.com>
2022-10-13 22:00:38 +0500

    PR15: fix CORS

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const NotFoundError = require('./errors/notFoundError');
 const auth = require('./middlewares/auth');
 const errorsHandler = require('./middlewares/errorsHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { cors } = require('./middlewares/cors');
+// const { cors } = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const corsOptions = {
+  origin: [
+    'http://uralyanka.mesto.nomoredomains.icu',
+    'https://uralyanka.mesto.nomoredomains.icu',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+};
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors(corsOptions));
+// app.use(cors);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
