cadd2775d29a9d8c7322b2f3e6e9e2a61e9fc0ed
Yana Evstegneeva <yanka.evste@gmail.com>
2022-10-14 13:41:51 +0500

    PR15: fix cors

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   origin: 'https://uralyanka.mesto.nomoredomains.icu',
+  credentials: true,
 }));
 // app.use(cors);
 
